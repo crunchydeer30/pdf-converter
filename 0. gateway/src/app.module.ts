0df19bf5d7cc2be31d5c08d7d12cacf2ef0c144e
PC <crunchydeer30@gmail.com>
2024-08-07 17:23:33 +0300

    gateway: setup ConfigModule

--- 0. gateway/src/app.module.ts
+++ 0. gateway/src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [ConfigModule.forRoot({
-    isGlobal: true
+    isGlobal: true,
+    validationSchema: Joi.object({
+      NODE_ENV: Joi.string()
+        .valid('development', 'production', 'test')
+        .default('development'),
+      PORT: Joi.number()
+        .default(8000),
+    })
   })],
   controllers: [AppController],
   providers: [AppService],
