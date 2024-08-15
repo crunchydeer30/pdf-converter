59beb8448e80a928ce343158dd858491a2c04d68
PC <crunchydeer30@gmail.com>
2024-08-15 11:12:35 +0300

    gateway: implement endpoint for upload ms office documents

--- 0. gateway/src/converter/converter.controller.ts
+++ 0. gateway/src/converter/converter.controller.ts
@@ -1,7 +1,19 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Inject,
+  UploadedFile,
+  UseInterceptors,
+  ParseFilePipe,
+  MaxFileSizeValidator,
+} from '@nestjs/common';
 import { ConverterService } from './converter.service';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { AlloweMimesValidator } from './validators/allowed-mimes.validator';
+import { OFFICE_MIMES } from './constants/office-mimes';
 
 @Controller('converter')
 export class ConverterController {
@@ -18,5 +30,21 @@ export class ConverterController {
   @Get('ping-worker')
   async pingWorker() {
     await this.converterService.pingWorker();
+  }
+
+  @Post('office-to-pdf')
+  @UseInterceptors(FileInterceptor('file'))
+  async officeToPdf(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 50 }),
+          new AlloweMimesValidator({ fileTypes: OFFICE_MIMES }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    return this.converterService.officeToPdf(file);
   }
 }
