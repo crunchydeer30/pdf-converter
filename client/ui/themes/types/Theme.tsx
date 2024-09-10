a565f69d056ad9f080ec8615630c759f686bb1e3
PC <crunchydeer30@gmail.com>
2024-09-10 18:52:10 +0300

    fix/client: include Theme interface changes

--- client/ui/themes/types/Theme.tsx
+++ client/ui/themes/types/Theme.tsx
@@ -1,5 +1,18 @@
 export interface Theme {
-  title: string;
   color: string;
   logo: string;
+  data: ThemeData;
+}
+
+interface ThemeData {
+  title: string;
+  subheader: string;
+  howTo: {
+    title: string;
+    steps: {
+      1: string;
+      2: string;
+      3: string;
+    };
+  };
 }
