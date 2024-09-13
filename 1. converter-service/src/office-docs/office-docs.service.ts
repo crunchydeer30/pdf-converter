76d613b3ef87cdd3279fa70f40b30fab5256d2a2
PC <crunchydeer30@gmail.com>
2024-09-13 19:35:39 +0300

    feat/converter-service: implement file link validation

--- 1. converter-service/src/office-docs/office-docs.service.ts
+++ 1. converter-service/src/office-docs/office-docs.service.ts
@@ -1,10 +1,12 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
 import { UtilsService } from 'src/utils/utils.service';
 import { v4 as uuid } from 'uuid';
 import { JobsService } from 'src/jobs/jobs.service';
+import { OFFICE_MIMES } from './constants/office-mimes';
+import * as path from 'path';
 
 @Injectable()
 export class OfficeDocsService {
@@ -25,5 +27,19 @@ export class OfficeDocsService {
     await this.jobsService.uploadToS3(jobId, file, fileMeta);
     await this.jobsService.create(jobId);
     return { message: 'File uploaded', jobId };
+  }
+
+  async officeToPdfLink(url: string) {
+    await this.validateLink(url);
+    return { message: 'File uploaded', jobId: url };
+  }
+
+  async validateLink(url: string) {
+    const mimetype = await this.utils.contentTypeFromUrl(url);
+
+    if (!OFFICE_MIMES.includes(mimetype))
+      throw new BadRequestException(
+        `Sorry, we are not able to convert ${path.extname(url)} file.`,
+      );
   }
 }
--- 1. converter-service/src/utils/utils.service.ts
+++ 1. converter-service/src/utils/utils.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import * as path from 'path';
 import { FileMetadata } from 'src/interfaces';
 import { RmqContext } from '@nestjs/microservices';
+import axios from 'axios';
 
 @Injectable()
 export class UtilsService {
@@ -17,5 +18,16 @@ export class UtilsService {
     const message = ctx.getMessage();
     const channel = ctx.getChannelRef();
     channel.ack(message);
+  }
+
+  async contentTypeFromUrl(url: string) {
+    try {
+      const response = await axios.head(url);
+      return response['content-type'];
+    } catch (error) {
+      if (error.response.status === 404) {
+        throw new BadRequestException(`Unable to access remote file: ${url}`);
+      }
+    }
   }
 }
