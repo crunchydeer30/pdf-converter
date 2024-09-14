efb0163357a9b7bfeb8cef523e94fb0ae10e22da
PC <crunchydeer30@gmail.com>
2024-09-14 16:13:40 +0300

    refactor/client: refactor Button styles

--- client/ui/button/Button.tsx
+++ client/ui/button/Button.tsx
@@ -9,24 +9,51 @@ interface ButtonProps extends ExtendableClassNames {
   href?: string;
   children: React.ReactNode;
   theme: Theme;
+  style?: "primary" | "secondary";
 }
 
 export function Button({ theme, href, children, ...props }: ButtonProps) {
-  const className = [
-    `text-${theme.color}`,
-    `bg-white`,
-    `border-2`,
-    `border-${theme.color}`,
-    "text-sm",
-    "font-bold",
-    "transition-all",
-    "rounded-full",
-    `hover:bg-${theme.color}`,
-    "hover:text-white",
-    "px-6",
-    "py-2",
-  ];
+  let className = [];
 
+  switch (props.style) {
+    case "secondary":
+      className = [
+        `text-${theme.color}`,
+        `bg-white`,
+        `border-2`,
+        `border-${theme.color}`,
+        "text-sm",
+        "font-bold",
+        "transition-all",
+        "rounded-full",
+        `hover:bg-${theme.color}`,
+        "hover:text-white",
+        "px-6",
+        "py-2",
+      ];
+      break;
+    default:
+      className = [
+        "flex",
+        "gap-2",
+        "relative",
+        "items-center",
+        "justify-center",
+        "text-white",
+        `bg-${theme.color}`,
+        `hover:bg-${theme.color}-light`,
+        "transition",
+        "py-6",
+        "px-10",
+        "rounded-full",
+        "text-xl",
+        "font-bold",
+        `disabled:bg-${theme.color}-light`,
+        `disabled:opacity-50`,
+        "disabled:cursor-not-allowed",
+        "cursor-pointer",
+      ];
+  }
   extendClassNames(props, className);
 
   if (href) {
