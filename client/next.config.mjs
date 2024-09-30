0eb73257d6579d7d9f124ca58199c84e1dc29c72
PC <crunchydeer30@gmail.com>
2024-09-30 14:45:56 +0300

    fix: fix client build workflow

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    env: {
+        NEXT_PUBLIC_API_GATEWAY: process.env.NEXT_PUBLIC_API_GATEWAY
+    }
 };
 
 export default nextConfig;
